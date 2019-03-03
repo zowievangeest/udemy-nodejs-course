9378d47c1a74821307b339e2949ce04a07aee898
Zowie van Geest <zowie@zowievangeest.nl>
2019-03-03 16:11:00 +0100

    chore(nodejs-learn-and-understand-course): Section 5 - 32

---
+++ Sectie 4 - Modules, Exports and Require/32 - Object Properties - Functions - Arrays/app.js
@@ -0,0 +1,26 @@
+var obj = {
+    greet: 'Hello'
+};
+
+console.log(obj.greet);
+console.log(obj['greet']);
+
+var prop = 'greet';
+console.log(obj[prop]);
+
+// Functions and arrays
+var arr = [];
+
+arr.push(function () {
+    console.log('Hello World 1');
+});
+arr.push(function () {
+    console.log('Hello World 2');
+});
+arr.push(function () {
+    console.log('Hello World 3');
+});
+
+arr.forEach(function (item) {
+    item();
+});
