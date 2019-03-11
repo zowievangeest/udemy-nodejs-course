71bb39f3fc409146a65d19c8e36fd24890707a6f
Zowie van Geest <zowie@zowievangeest.nl>
2019-03-11 21:08:24 +0100

    chore(nodejs-learn-and-understand-course): Section 7 - 61

---
+++ Sectie 7 - HTTP and being a Web Server/61 - Streams and Performance/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+
+}).listen(1337, '127.0.0.1');
