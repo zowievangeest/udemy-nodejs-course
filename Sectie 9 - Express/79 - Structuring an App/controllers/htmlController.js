672aadb4748ab4ee2f45bb732fa61a4bfb3a2adc
Zowie van Geest <zowie@zowievangeest.nl>
2019-03-12 21:41:10 +0100

    chore(nodejs-learn-and-understand-course): Section 9 - 79

---
+++ Sectie 9 - Express/79 - Structuring an App/controllers/htmlController.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function (app) {
+
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+
+    app.get('/person/:id', function (req, res) {
+        res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+    });
+
+    app.post('/person', urlencodedParser, function (req, res) {
+        res.send('Thanks!');
+        console.log(req.body.firstname);
+        console.log(req.body.lastname);
+    });
+
+};
